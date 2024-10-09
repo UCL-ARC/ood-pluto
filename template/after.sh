05ccefe2b1a02e39e9ee4f995c67331b9b03de93
Ian Kirker <ian.kirker@gmail.com>
2024-10-09 12:40:28 +0100

    Initial import of Pluto fork of Jupyter app.

---
+++ template/after.sh
@@ -0,0 +1,14 @@
+# Wait for the Pluto server to start
+set -x
+echo "Waiting for Pluto server to open port ${port}..."
+echo "TIMING - Starting wait at: $(date)"
+if wait_until_port_used "${host}:${port}" 240; then
+  echo "Discovered Pluto server listening on port ${port}!"
+  echo "TIMING - Wait ended at: $(date)"
+else
+  echo "Timed out waiting for Pluto server to open port ${port}!"
+  echo "TIMING - Wait ended at: $(date)"
+  pkill -P ${SCRIPT_PID}
+  clean_up 1
+fi
+sleep 2
